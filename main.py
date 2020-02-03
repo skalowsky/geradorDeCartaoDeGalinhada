42aad1ad3402b1f960467bdc43ae4936db8c6c80
Marmitt <mmarmit@ad.esi.adp.com>
2020-02-03 19:52:33 -0300

    variaveis globais

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from PIL import Image, ImageDraw, ImageFont
 
+#Variaveis
+FONTE_REGULAR = 'fonte/Arimo-Regular.ttf'
+FONTE_NEGRITO = 'fonte/Arimo-Bold.ttf'
+DATA = '08/02/2020'
+VALOR = '10,00 (dez reais)'
+NOME_SEDE = 'Associação dos Moradores do Bairro Aviação'
+ENDERECO = 'Rua Treze de Maio, 160 - B. Aviação'
+CIDADE_ESTADO = 'Venâncio Aires - RS'
+PREMIO = 'SERÃO SORTEADOS DOIS PERNIS SUÍNOS COM PELE'
+PREMIACAO_FEMININO = False
+LOGO = 'logo.png'
+TAMANHO_LOGO = 60
+NUMERO_DE_CARTOES = 250
+
+#Constantes 
 A4 = (595, 842)
 NUM_CARTAO_FOLHA = 5
 
@@ -41,27 +56,27 @@ def criarLinhas(cartao: Image) -> Image:
 
 
 def criarDescricoesEsquerda(cartao: Image, numero: int, data: str, valor: str, nome: str, endereco: str, cidade: str) -> Image:
-  fonteNumero = ImageFont.truetype("fonte/Arimo-Regular.ttf", 20)
-  fonteTitulo = ImageFont.truetype("fonte/Arimo-Bold.ttf", 16)
-  fonteDescricao = ImageFont.truetype("fonte/Arimo-Regular.ttf", 11)
+  fonteNumero = ImageFont.truetype(FONTE_REGULAR, 20)
+  fonteTitulo = ImageFont.truetype(FONTE_NEGRITO, 16)
+  fonteDescricao = ImageFont.truetype(FONTE_REGULAR, 11)
  
   escrita = ImageDraw.Draw(cartao)
 
   #Escreve o numero
-  escrita.text((CARTAO[0] * 0.22, CARTAO[1] * 0.1),"Nº " + str(numero).zfill(3), PRETO, font = fonteNumero)
+  escrita.text((CARTAO[0] * 0.22, CARTAO[1] * 0.1),'Nº ' + str(numero).zfill(3), PRETO, font = fonteNumero)
 
   #Escreve 'Cartão de Galinhada' e sublinha
-  escrita.text((CARTAO[0] * 0.12, CARTAO[1] * 0.25),"CARTÃO DE GALINHADA" , PRETO, font = fonteTitulo)
+  escrita.text((CARTAO[0] * 0.12, CARTAO[1] * 0.25),'CARTÃO DE GALINHADA' , PRETO, font = fonteTitulo)
   escrita.line((CARTAO[0] * 0.12, CARTAO[1] * 0.35, CARTAO[0] * 0.46, CARTAO[1] * 0.35), PRETO , width = 2)
 
   #Escreve a data do evento
-  escrita.text((CARTAO[0] * 0.10, CARTAO[1] * 0.43),"Data: " + data + " (Sábado ao meio dia)", PRETO, font = fonteDescricao)
+  escrita.text((CARTAO[0] * 0.10, CARTAO[1] * 0.43),'Data: ' + data + ' (Sábado ao meio dia)', PRETO, font = fonteDescricao)
 
   #Escreve o valor de cada cartão
-  escrita.text((CARTAO[0] * 0.15, CARTAO[1] * 0.55),"Valor: R$ " + valor, PRETO, font = fonteDescricao)
+  escrita.text((CARTAO[0] * 0.15, CARTAO[1] * 0.55),'Valor: R$ ' + valor, PRETO, font = fonteDescricao)
 
   #Escreve o nome da sede
-  escrita.text((CARTAO[0] * 0.05, CARTAO[1] * 0.67),"Local: " + nome, PRETO, font = fonteDescricao)
+  escrita.text((CARTAO[0] * 0.05, CARTAO[1] * 0.67),'Local: ' + nome, PRETO, font = fonteDescricao)
 
   #Escreve o rua e o numero
   escrita.text((CARTAO[0] * 0.12, CARTAO[1] * 0.79), endereco, PRETO, font = fonteDescricao)
@@ -74,33 +89,33 @@ def criarDescricoesEsquerda(cartao: Image, numero: int, data: str, valor: str, n
 
 
 def criarDescricoesDireita(cartao: Image, numero: int, premio: str, descricaoNoFeminino: bool) -> Image:
-  fonteNumero = ImageFont.truetype("fonte/Arimo-Regular.ttf", 20)
-  fonteDescricao = ImageFont.truetype("fonte/Arimo-Bold.ttf", 10)
-  fontePreencher = ImageFont.truetype("fonte/Arimo-Regular.ttf", 14)
+  fonteNumero = ImageFont.truetype(FONTE_REGULAR, 20)
+  fonteDescricao = ImageFont.truetype(FONTE_NEGRITO, 10)
+  fontePreencher = ImageFont.truetype(FONTE_REGULAR, 14)
 
   escrita = ImageDraw.Draw(cartao)
 
   #Escreve descrição padrão
-  escrita.text((CARTAO[0] * 0.55, CARTAO[1] * 0.1), "PREENCHA E CONCORRA A DOIS SORTEIOS", PRETO, font = fonteDescricao)
-  escrita.text((CARTAO[0] * 0.65, CARTAO[1] * 0.2), "NO DIA DA GALINHADA", PRETO, font = fonteDescricao)
+  escrita.text((CARTAO[0] * 0.55, CARTAO[1] * 0.1), 'PREENCHA E CONCORRA A DOIS SORTEIOS', PRETO, font = fonteDescricao)
+  escrita.text((CARTAO[0] * 0.65, CARTAO[1] * 0.2), 'NO DIA DA GALINHADA', PRETO, font = fonteDescricao)
 
   #Escreve o premio
   escrita.text((CARTAO[0] * 0.52, CARTAO[1] * 0.35), premio, PRETO, font = fonteDescricao)
   if descricaoNoFeminino:
-    escrita.text((CARTAO[0] * 0.65, CARTAO[1] * 0.45), "(UMA POR CARTÃO)", PRETO, font = fonteDescricao)
+    escrita.text((CARTAO[0] * 0.65, CARTAO[1] * 0.45), '(UMA POR CARTÃO)', PRETO, font = fonteDescricao)
   else:
-    escrita.text((CARTAO[0] * 0.65, CARTAO[1] * 0.45), "(UM POR CARTÃO)", PRETO, font = fonteDescricao)
+    escrita.text((CARTAO[0] * 0.65, CARTAO[1] * 0.45), '(UM POR CARTÃO)', PRETO, font = fonteDescricao)
 
   #Escreve o nome
-  escrita.text((CARTAO[0] * 0.52, CARTAO[1] * 0.6), "Nome: ", PRETO, font = fontePreencher)
+  escrita.text((CARTAO[0] * 0.52, CARTAO[1] * 0.6), 'Nome: ', PRETO, font = fontePreencher)
   escrita.line((CARTAO[0] * 0.59, CARTAO[1] * 0.68, CARTAO[0] * 0.95, CARTAO[1] * 0.68), PRETO , width = 1) 
 
   #Escreve o telefone
-  escrita.text((CARTAO[0] * 0.52, CARTAO[1] * 0.72), "Telefone: ", PRETO, font = fontePreencher)
+  escrita.text((CARTAO[0] * 0.52, CARTAO[1] * 0.72), 'Telefone: ', PRETO, font = fontePreencher)
   escrita.line((CARTAO[0] * 0.625, CARTAO[1] * 0.8, CARTAO[0] * 0.95, CARTAO[1] * 0.8), PRETO , width = 1) 
 
   #Escreve o numero
-  escrita.text((CARTAO[0] * 0.7, CARTAO[1] * 0.85),"Nº " + str(numero).zfill(3), PRETO, font = fonteNumero)
+  escrita.text((CARTAO[0] * 0.7, CARTAO[1] * 0.85),'Nº ' + str(numero).zfill(3), PRETO, font = fonteNumero)
 
   return cartao
 
@@ -112,21 +127,20 @@ def criarCartao(dimensao, logo: Image, numero: int) -> Image:
   logo = redimencionarLogo(logo, 60)
   criarLinhas(cartao)
 
-  criarDescricoesEsquerda(
-    cartao, 
-    numero,
-    "01/01/2020",
-    "10,00 (dez reais)",
-    "Associação dos Moradores do Bairro Aviação",
-    "Rua Treze de Maio, 160 - B. Aviação",
-    "Venâncio Aires - RS"
+  criarDescricoesEsquerda(    
+    cartao,    
+    numero,    
+    DATA,    
+    VALOR,    
+    NOME_SEDE,    
+    ENDERECO,CIDADE_ESTADO  
   )
 
   criarDescricoesDireita(
     cartao,
     numero,
-    "SERÃO SORTEADOS DOIS PERNIS SUÍNOS COM PELE",
-    False
+    PREMIO,
+    PREMIACAO_FEMININO
   )
 
   cartao.paste(logo, (5, 8))
@@ -135,16 +149,13 @@ def criarCartao(dimensao, logo: Image, numero: int) -> Image:
 
 
 def criaPagina(numeroDaPagina: int) -> Image:
-  logo = Image.open('logo.png')
+  logo = Image.open(LOGO)
 
   #Tamanho A4 com o fundo branco
   pagina: Image = criarFolhaA4()
   
   for numero in range(5):
-    if numeroDaPagina == 1:
-      numeroDoCartao = (numero + 1)
-    else:
-      numeroDoCartao = (numero + 1) + ( (numeroDaPagina - 1) * 5)
+    numeroDoCartao = (numero + 1) + ( (numeroDaPagina - 1) * 5)
 
     pagina.paste(criarCartao(CARTAO, logo, numeroDoCartao), (0, numero *  CARTAO[1]))
 
@@ -152,20 +163,18 @@ def criaPagina(numeroDaPagina: int) -> Image:
 
 
 
-
 def start():
   pagina: Image = criaPagina(1)
+  numeroDePaginas = (NUMERO_DE_CARTOES // 5) + 1
 
   paginas = []
 
-  for numeroDaPagina in range(2, 51, 1):    
+  for numeroDaPagina in range(2, numeroDePaginas, 1):
     paginas.append(criaPagina(numeroDaPagina))
 
   pagina.save('cartao.pdf', 'pdf', save_all = True, append_images = paginas)
 
 
 
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
   start()
